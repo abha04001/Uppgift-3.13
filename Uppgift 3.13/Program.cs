85c469d4e0069df76dffab6a7e3712e4873cbb19
caspian <casa04001@utb.vaxjo.se>
2022-09-29 13:57:44 +0200

    Add project files.

---
+++ Uppgift 3.13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_3._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ditt ålder om du vill delta i tävlingen OBS måste vara mellan 16 - 19");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("Du får INTE delta i tävlingen");
+            }
+
+        }
+    }
+}
